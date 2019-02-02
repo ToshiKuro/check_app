c8d2a4abfadd81b69742982286b164b681e06452
Toshihiro_Kurosawa <toshi.kurochin@gmail.com>
2019-02-02 22:18:46 +0900

    create lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/show'
+  get 'lists/create'
   get 'checklists/index'
   get 'checklists/new'
   get 'checklists/show'
