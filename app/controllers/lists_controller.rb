31d233008327291efd49b4fe29214c18de01d463
Toshihiro_Kurosawa <toshi.kurochin@gmail.com>
2019-02-06 16:36:02 +0900

    add list destroy function

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,8 @@
 class ListsController < ApplicationController
+
+  require 'pry'
+    # render plain: @list.inspect
+
   def index
   end
 
@@ -6,11 +10,31 @@ class ListsController < ApplicationController
   end
 
   def show
+    @lists = List.where(name: params[:name])
   end
 
   def create
     list = List.create(list_params)
-    redirect_to action: 'show'
+    redirect_to action: 'show', name: list.name
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to action: 'show'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to action: 'show', name: list.name
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
 
   get 'lists/index'
   get 'lists/new'
-  get 'lists/show'
-  post "lists" => "lists#create"
+  get 'lists/:name', to: 'lists#show'
+  # get 'lists/show/:id', to: 'lists#show', as: 'list_show'
+  post 'lists', to: 'lists#create'
+  post 'lists/edit', to: 'lists#edit'
+  put 'lists', to: 'lists#update'
+  delete 'lists/delete/:id', to: 'lists#destroy', as: 'list_delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
