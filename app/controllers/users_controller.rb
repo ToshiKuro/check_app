3e25721df09ea369800e133a7afee245b6077d3c
Toshihiro_Kurosawa <toshi.kurochin@gmail.com>
2019-02-08 08:27:19 +0900

    add user's check list edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
   end
@@ -17,9 +18,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    # render plain: @user.inspect
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to action: 'index'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -30,4 +39,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :list1, :list2, :list3, :list4, :list5, :list6, :list7, :list8)
   end
+
 end
