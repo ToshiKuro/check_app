152771178d65107d5eff348014b8112a5cdd1531
Toshihiro_Kurosawa <toshi.kurochin@gmail.com>
2019-02-17 20:34:11 +0900

    @list change to list in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,86 +6,86 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# @list         = List.new
-# @list.name    = 'IJ201'
-# @list.item    = 'BRFGシート'
-# @list.address = ''
-# @list.save
+# list         = List.new
+# list.name    = 'IJ201'
+# list.item    = 'BRFGシート'
+# list.address = ''
+# list.save
 
-@list         = List.new
-@list.name    = 'IJ201'
-@list.item    = '飛行実施計画'
-@list.address = '/uploads/upload_file/file/4/pln201.pdf'
-@list.save
+list         = List.new
+list.name    = 'IJ201'
+list.item    = '飛行実施計画'
+list.address = '/uploads/upload_file/file/4/pln201.pdf'
+list.save
 
-# @list         = List.new
-# @list.name    = 'IJ201'
-# @list.item    = '飛行計画'
-# @list.address = ''
-# @list.save
+# list         = List.new
+# list.name    = 'IJ201'
+# list.item    = '飛行計画'
+# list.address = ''
+# list.save
 
-@list         = List.new
-@list.name    = 'IJ201'
-@list.item    = 'NOTAM'
-@list.address = '/uploads/upload_file/file/2/NOTAM201.pdf'
-@list.save
+list         = List.new
+list.name    = 'IJ201'
+list.item    = 'NOTAM'
+list.address = '/uploads/upload_file/file/2/NOTAM201.pdf'
+list.save
 
-# @list         = List.new
-# @list.name    = 'IJ201'
-# @list.item    = 'TAF/METER'
-# @list.address = ''
-# @list.save
+# list         = List.new
+# list.name    = 'IJ201'
+# list.item    = 'TAF/METER'
+# list.address = ''
+# list.save
 
-@list         = List.new
-@list.name    = 'IJ201'
-@list.item    = 'FBJP'
-@list.address = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
-@list.save
+list         = List.new
+list.name    = 'IJ201'
+list.item    = 'FBJP'
+list.address = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
+list.save
 
-@list         = List.new
-@list.name    = 'IJ201'
-@list.item    = '中国重要天気'
-@list.address = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
-@list.save
+list         = List.new
+list.name    = 'IJ201'
+list.item    = '中国重要天気'
+list.address = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+list.save
 
-# @list         = List.new
-# @list.name    = 'IJ202'
-# @list.item    = 'BRFGシート'
-# @list.address = ''
-# @list.save
+# list         = List.new
+# list.name    = 'IJ202'
+# list.item    = 'BRFGシート'
+# list.address = ''
+# list.save
 
-@list         = List.new
-@list.name    = 'IJ202'
-@list.item    = '飛行実施計画'
-@list.address = '/uploads/upload_file/file/5/pln202.pdf'
-@list.save
+list         = List.new
+list.name    = 'IJ202'
+list.item    = '飛行実施計画'
+list.address = '/uploads/upload_file/file/5/pln202.pdf'
+list.save
 
-# @list         = List.new
-# @list.name    = 'IJ202'
-# @list.item    = '飛行計画'
-# @list.address = ''
-# @list.save
+# list         = List.new
+# list.name    = 'IJ202'
+# list.item    = '飛行計画'
+# list.address = ''
+# list.save
 
-@list         = List.new
-@list.name    = 'IJ202'
-@list.item    = 'NOTAM'
-@list.address = '/uploads/upload_file/file/3/NOTAM202.pdf'
-@list.save
+list         = List.new
+list.name    = 'IJ202'
+list.item    = 'NOTAM'
+list.address = '/uploads/upload_file/file/3/NOTAM202.pdf'
+list.save
 
-# @list         = List.new
-# @list.name    = 'IJ202'
-# @list.item    = 'TAF/METER'
-# @list.address = ''
-# @list.save
+# list         = List.new
+# list.name    = 'IJ202'
+# list.item    = 'TAF/METER'
+# list.address = ''
+# list.save
 
-@list         = List.new
-@list.name    = 'IJ202'
-@list.item    = 'FBJP'
-@list.address = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
-@list.save
+list         = List.new
+list.name    = 'IJ202'
+list.item    = 'FBJP'
+list.address = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
+list.save
 
-@list         = List.new
-@list.name    = 'IJ202'
-@list.item    = '毎次大気解析'
-@list.address = 'http://www.data.jma.go.jp/airinfo/data/pict/maiji/WANLF145_RJTD_20190216100000.PNG'
-@list.save
+list         = List.new
+list.name    = 'IJ202'
+list.item    = '毎次大気解析'
+list.address = 'http://www.data.jma.go.jp/airinfo/data/pict/maiji/WANLF145_RJTD_20190216100000.PNG'
+list.save
