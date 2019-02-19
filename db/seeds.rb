354f5e387cb8b9ba9666cda9209d4072433f5080
Toshihiro_Kurosawa <toshi.kurochin@gmail.com>
2019-02-19 09:08:18 +0900

    change seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,86 +6,171 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# list         = List.new
-# list.name    = 'IJ201'
-# list.item    = 'BRFGシート'
-# list.address = ''
-# list.save
 
+# User登録
+user         = User.new
+user.name    = '鈴木一郎'
+user.save
+
+user         = User.new
+user.name    = '田中二郎'
+user.save
+
+user         = User.new
+user.name    = '佐藤三郎'
+user.save
+
+user         = User.new
+user.name    = '高橋四郎'
+user.save
+
+user         = User.new
+user.name    = '伊藤五郎'
+user.save
+
+user         = User.new
+user.name    = '山本六郎'
+user.save
+
+
+# List登録
 list         = List.new
-list.name    = 'IJ201'
-list.item    = '飛行実施計画'
-list.address = '/uploads/upload_file/file/4/pln201.pdf'
-list.save
-
-# list         = List.new
-# list.name    = 'IJ201'
-# list.item    = '飛行計画'
-# list.address = ''
-# list.save
-
-list         = List.new
-list.name    = 'IJ201'
-list.item    = 'NOTAM'
-list.address = '/uploads/upload_file/file/2/NOTAM201.pdf'
-list.save
-
-# list         = List.new
-# list.name    = 'IJ201'
-# list.item    = 'TAF/METER'
-# list.address = ''
-# list.save
-
-list         = List.new
-list.name    = 'IJ201'
-list.item    = 'FBJP'
-list.address = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
+list.name    = 'a101'
 list.save
 
 list         = List.new
-list.name    = 'IJ201'
-list.item    = '中国重要天気'
-list.address = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
-list.save
-
-# list         = List.new
-# list.name    = 'IJ202'
-# list.item    = 'BRFGシート'
-# list.address = ''
-# list.save
-
-list         = List.new
-list.name    = 'IJ202'
-list.item    = '飛行実施計画'
-list.address = '/uploads/upload_file/file/5/pln202.pdf'
-list.save
-
-# list         = List.new
-# list.name    = 'IJ202'
-# list.item    = '飛行計画'
-# list.address = ''
-# list.save
-
-list         = List.new
-list.name    = 'IJ202'
-list.item    = 'NOTAM'
-list.address = '/uploads/upload_file/file/3/NOTAM202.pdf'
-list.save
-
-# list         = List.new
-# list.name    = 'IJ202'
-# list.item    = 'TAF/METER'
-# list.address = ''
-# list.save
-
-list         = List.new
-list.name    = 'IJ202'
-list.item    = 'FBJP'
-list.address = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
+list.name    = 'a102'
 list.save
 
 list         = List.new
-list.name    = 'IJ202'
-list.item    = '毎次大気解析'
-list.address = 'http://www.data.jma.go.jp/airinfo/data/pict/maiji/WANLF145_RJTD_20190216100000.PNG'
+list.name    = 'b201'
 list.save
+
+list         = List.new
+list.name    = 'b202'
+list.save
+
+
+# （List）Item登録
+item         = Item.new
+item.list_id = 1
+item.item    = '計画書'
+item.path    = '/uploads/upload_file/file/4/pln201.pdf'
+item.save
+
+item         = Item.new
+item.list_id = 1
+item.item    = 'INFORMATION'
+item.path    = '/uploads/upload_file/file/2/NOTAM201.pdf'
+item.save
+
+item         = Item.new
+item.list_id = 1
+item.item    = '資料１'
+item.path    = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
+item.save
+
+item         = Item.new
+item.list_id = 1
+item.item    = '資料２'
+item.path    = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+item.save
+
+item         = Item.new
+item.list_id = 1
+item.item    = 'メモ'
+item.path    = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+item.save
+
+
+item         = Item.new
+item.list_id = 2
+item.item    = '計画書'
+item.path    = '/uploads/upload_file/file/4/pln201.pdf'
+item.save
+
+item         = Item.new
+item.list_id = 2
+item.item    = 'INFORMATION'
+item.path    = '/uploads/upload_file/file/2/NOTAM201.pdf'
+item.save
+
+item         = Item.new
+item.list_id = 2
+item.item    = '資料１'
+item.path    = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
+item.save
+
+item         = Item.new
+item.list_id = 2
+item.item    = '資料２'
+item.path    = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+item.save
+
+item         = Item.new
+item.list_id = 2
+item.item    = 'メモ'
+item.path    = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+item.save
+
+
+item         = Item.new
+item.list_id = 3
+item.item    = '計画書'
+item.path    = '/uploads/upload_file/file/4/pln201.pdf'
+item.save
+
+item         = Item.new
+item.list_id = 3
+item.item    = 'INFORMATION'
+item.path    = '/uploads/upload_file/file/2/NOTAM201.pdf'
+item.save
+
+item         = Item.new
+item.list_id = 3
+item.item    = '資料１'
+item.path    = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
+item.save
+
+item         = Item.new
+item.list_id = 3
+item.item    = '資料２'
+item.path    = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+item.save
+
+item         = Item.new
+item.list_id = 3
+item.item    = 'メモ'
+item.path    = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+item.save
+
+
+item         = Item.new
+item.list_id = 4
+item.item    = '計画書'
+item.path    = '/uploads/upload_file/file/4/pln201.pdf'
+item.save
+
+item         = Item.new
+item.list_id = 4
+item.item    = 'INFORMATION'
+item.path    = '/uploads/upload_file/file/2/NOTAM201.pdf'
+item.save
+
+item         = Item.new
+item.list_id = 4
+item.item    = '資料１'
+item.path    = 'https://www.data.jma.go.jp/airinfo/data/pict/fbjp/fbjp.png'
+item.save
+
+item         = Item.new
+item.list_id = 4
+item.item    = '資料２'
+item.path    = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+item.save
+
+item         = Item.new
+item.list_id = 4
+item.item    = 'メモ'
+item.path    = '/uploads/upload_file/file/1/QMCD98_RJTT.png'
+item.save
