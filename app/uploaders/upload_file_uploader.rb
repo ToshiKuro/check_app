9123c48385636cb11b337e4262adae94048347f4
Toshihiro_Kurosawa <toshi.kurochin@gmail.com>
2019-04-24 15:10:07 +0900

    img test5

--- app/uploaders/upload_file_uploader.rb
+++ app/uploaders/upload_file_uploader.rb
@@ -4,18 +4,18 @@ class UploadFileUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
-  # process :convert => 'pdf' # 画像の保存形式
+  process :convert => 'pdf' # 画像の保存形式
   process :tags => ['image'] # 保存時に添付されるタグ（管理しやすいように適宜変更しましょう）
   process :resize_to_limit => [700, 700] # 任意でリサイズの制限
 
   # 保存する画像の種類をサイズ別に設定
-  version :standard do
-    process :resize_to_fill => [100, 150, :north]
-  end
+  # version :standard do
+  #   process :resize_to_fill => [100, 150, :north]
+  # end
 
-  version :thumb do
-    process :resize_to_fit => [50, 50]
-  end
+  # version :thumb do
+  #   process :resize_to_fit => [50, 50]
+  # end
 
   def extension_whitelist
     %w(pdf png jpg)
